3ebf17ed33bdaa29785fab106b134e4bd96040f2
migue <miguel.landa.villa@gmail.com>
2022-04-16 23:41:35 -0500

    Controladores, vistas y fotos agragadas inicio

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css\headerStyle.css" rel="stylesheet">
+        <title>Questions Challenge</title>
+    </head>
+    <body class="antialiased">
+        <header>
+            <img class="logo" src="images\questionChallenge3.png" alt="Foto de logo" />
+            <nav>
+                <ul class="nav_links">
+                    <li><a href="#" >Categorias</a></li>
+                    <li><a href="#" >Como Jugar</a></li>
+                    <li><a href="#" >Preguntas Frecuentes</a></li>
+
+                </ul>
+            </nav>
+            <a class="cta" href="#" name ="adminHome"><button>Admin</button></a>
+        </header>
+        <button type="submit">Jugar</button>
+    
+    </body>
+</html>
