6f9129eeef1303dc044b2de559f9699c00450a0e
migue <miguel.landa.villa@gmail.com>
2022-05-16 18:20:19 -0500

    Avances pantalla juego, preguntas aleatorias

--- app/Http/Controllers/JuegoController.php
+++ app/Http/Controllers/JuegoController.php
@@ -4,17 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\pregunta;
+use App\Models\respuesta;
 use Illuminate\Support\Facades\DB;
 
 class JuegoController extends Controller
 {
     public function create(){
-        $preguntas = DB::table('preguntas')->get('*')->where('idTematica','=',1);
-        return view('JuegoView', compact('preguntas'));
+        $preguntas = DB::table('preguntas')
+            ->get('*')
+            ->where('idTematica','=',1);
+        $respuestasc = DB::table('respuestas')
+            ->get('*')
+            ->where('idTematica','=',1);
+        return view('JuegoView', compact('preguntas','respuestasc'));
     }
 
     public function showJuego(Request $request){
-        $preguntas = DB::table('preguntas')->get('*')->where('idTematica','=',$request->Tematica);
+        $preguntas = DB::table('preguntas')
+        ->get('*')
+        ->where('idTematica','=',$request->Tematica);
+        
+        $respuestasc = DB::table('respuestas')
+            ->select('*')
+            ->join('preguntas','preguntas.idPregunta','=','respuestas.idPregunta')
+            ->where('preguntas.idTematica','=',$request->Tematica)->get();
+
+        $contadorP = DB::table('preguntas')->count();
+
+
+        
+
+
+
         $Tematica = $request->Tematica;
         $NumJug = $request->NumJugadores;
         $NombreJu1 = $request->Nombrejugador1;
@@ -33,26 +54,28 @@ class JuegoController extends Controller
                 $ImgJugadores = [$ImagenJu1,$ImagenJu2,$ImagenJu3];
                 $NameJugadores = [$NombreJu1,$NombreJu2,$NombreJu3];
                 $PuntajeJugadores = [$puntajeJugador1,$puntajeJugador2,$puntajeJugador3];
-                return view('JuegoView', compact('Tematica','NumJug','ImgJugadores', 'NameJugadores','PuntajeJugadores','preguntas'));
+                return view('JuegoView', compact('Tematica','NumJug','ImgJugadores', 'NameJugadores','PuntajeJugadores','preguntas','respuestasc','contadorP'));
             } 
             else {
                 $ImgJugadores = [$ImagenJu1,$ImagenJu2];
                 $NameJugadores = [$NombreJu1,$NombreJu2];
                 $PuntajeJugadores = [$puntajeJugador1,$puntajeJugador2];
-                return view('JuegoView', compact('Tematica','NumJug','ImgJugadores', 'NameJugadores','PuntajeJugadores','preguntas'));
+                return view('JuegoView', compact('Tematica','NumJug','ImgJugadores', 'NameJugadores','PuntajeJugadores','preguntas','respuestasc','contadorP'));
             }
         }
         else{
             $ImgJugadores = [$ImagenJu1];
             $NameJugadores = [$NombreJu1];
             $PuntajeJugadores = [$puntajeJugador1];
-            return view('JuegoView', compact('Tematica','NumJug','ImgJugadores', 'NameJugadores','PuntajeJugadores','preguntas'));
+            return view('JuegoView', compact('Tematica','NumJug','ImgJugadores', 'NameJugadores','PuntajeJugadores','preguntas','respuestasc'));
         }
+
     }
 
     public function preguntas(Request $request){
         $preguntas = pregunta::all();
-        return view('JuegoView', compact('preguntas'));
+        $respuestasc = respuesta::all();
+        return view('JuegoView', compact('preguntas','respuestasc'));
         // return redirect()->route('Juego');
     }
 }
