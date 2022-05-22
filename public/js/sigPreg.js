50001f8d9faa336714595d161f181f622119bfd5
migue <miguel.landa.villa@gmail.com>
2022-05-22 18:34:48 -0500

    Corecciones css y checado de preguntas

---
+++ public/js/sigPreg.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const res = document.getElementsByClassName("respuesta");
+
+console.log(res);
--- resources/views/JuegoView.blade.php
+++ resources/views/JuegoView.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" type="image/png" href="{{ url('images\questionChallenge3.png') }}">
-    <link href="{{ url('css\JuegoStyle.css') }}" rel="stylesheet">
+    <!-- <link href="css/JuegoStyle.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="css/JuegoStyle.css">
+    <script type="text/javascript" src="js/sigPreg.js"></script>
     <title>Question Challenge</title>
 </head>
 
@@ -26,7 +28,6 @@
 
         <h4>{{ $preguntas2[$random]->descripcion }}</h4>
         
-
         @if (($random + 1) % 3 == 2)
             <input class="respuesta" type="button" value="{{ $preguntas2[$random]->respuesta1 }}">
             <input class="respuesta" type="button" value="{{ $preguntas2[$random]->respuesta2 }}">
@@ -36,19 +37,18 @@
             <input class="respuesta" type="button" value="{{ $preguntas2[$random]->respuestacorrecta }}">
             <input class="respuesta" type="button" value="{{ $preguntas2[$random]->respuesta2 }}">
         @else
-            <input class="respuesta" type="button" value="{{ $preguntas2[$random]->respuestacorrecta }}">
-            <input class="respuesta" type="button" value="{{ $preguntas2[$random]->respuesta2 }}">
-            <input class="respuesta" type="button" value="{{ $preguntas2[$random]->respuesta1 }}">
+        <input class="respuesta" type="button" value="{{ $preguntas2[$random]->respuestacorrecta }}">
+        <input class="respuesta" type="button" value="{{ $preguntas2[$random]->respuesta2 }}">
+        <input class="respuesta" type="button" value="{{ $preguntas2[$random]->respuesta1 }}">
         @endif
 
 
 
-        <input id="enviar" type="submit" value="Enviar">
+        <input class="enviar" type="submit" value="Enviar">
     </form>
 
-
-    <figure id="Tablero">
-        <img src="{{ url('images\Pantallas\Pantalla1.png') }}" alt="Tablero">
+    <figure class='tablero'>
+        <img id="imgTab" src="{{ url('images\Pantallas\Pantalla1.png') }}" alt="tablero de juego">
     </figure>
 
     <div id="Jugadores">
@@ -57,9 +57,9 @@
         @endphp
         @foreach ($ImgJugadores as $item)
             <figure class="avatares">
+                <label for="">nombre: {{ $NameJugadores[$i] }}</label>
                 <img src="{{ $item }}" alt="Avatar{{ $i + 1 }}">
                 <label for="">puntuacion: {{ $PuntajeJugadores[$i] }}</label>
-                <label for="">nombre: {{ $NameJugadores[$i] }}</label>
             </figure>
             @php
                 $i++;
@@ -67,47 +67,35 @@
             @endphp
         @endforeach
 
-        {{-- <figure class="avatares">
-                <img src="images/avatares/kemonito.png" alt="Avatar1">
-                <label for="">puntuacion: 100</label>
-            </figure>
-            <figure class="avatares">
-                <img src="images/avatares/robot.png" alt="Avatar1">
-                <label for="">puntuacion: 100</label>
-            </figure> --}}
-        <form id="Form" class="pr-8" action="{{ url('GuardarPartida', $ii) }}"
-            method="post">
-            @csrf
-            @php
-                $i = 0;
-            @endphp
-            @foreach ($ImgJugadores as $item)
-
-                    <input hidden name="imagen[]" value="{{ $item }}"/>
-                    <input hidden name="puntaje[]" value="{{ $PuntajeJugadores[$i] }}" />
-                    <input hidden name="nombre[]" value="{{ $NameJugadores[$i] }}"/>
+        <div id="Acciones">
+            <form class="Accion" class="pr-8" action="{{ url('GuardarPartida', $ii) }}"
+                method="post">
+                @csrf
                 @php
-                    $i++;
+                    $i = 0;
                 @endphp
+                @foreach ($ImgJugadores as $item)
 
-            @endforeach
-            <input hidden name="tematica" value="{{ $preguntas2[$random]->tematica_id }}"/>
-            <input id="enviar" type="submit" value="Guardar">
-        </form>
-        <form id="Form" action="{{ route('AbandonarPartida') }}" method="post">
-            @csrf
+                        <input hidden name="imagen[]" value="{{ $item }}"/>
+                        <input hidden name="puntaje[]" value="{{ $PuntajeJugadores[$i] }}" />
+                        <input hidden name="nombre[]" value="{{ $NameJugadores[$i] }}"/>
+                    @php
+                        $i++;
+                    @endphp
 
-            <input id="enviar" type="submit" value="Abandonar">
-        </form>
+                @endforeach
+                <input hidden name="tematica" value="{{ $preguntas2[$random]->tematica_id }}"/>
+                <input class="enviar" type="submit" value="Guardar">
+            </form>
+
+            <form class="Accion" action="{{ route('AbandonarPartida') }}" method="post">
+                @csrf
+                <input class="enviar" type="submit" value="Abandonar">
+            </form>
+        </div>
+        
     </div>
 
-
-    <!-- <div class="flex justify-between m-4">
-        <div class="flex flex-col h-full max-w-lg mx-auto bg-gray-800 rounded-lg">
-            <img class="rounded-lg rounded-b-none"
-            src="{{ url('images\Pantallas\Pantalla1.png') }}" alt="Tablero" loading="lazy" />
-        </div>
-    </div> -->
 </body>
 
 </html>
